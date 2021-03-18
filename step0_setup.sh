266d1f435e3683373615fbe57aaabc9f35713447
jasondeglint <41922820+jasondeglint@users.noreply.github.com>
2021-03-18 17:42:58 -0400

    Update step0_setup.sh

--- step0_setup.sh
+++ step0_setup.sh
@@ -5,8 +5,8 @@
 ### basis setup ### 
 
 # to get your administation in order
-sudo apt-get -y update
-sudo apt-get -y upgrade
+sudo apt-get update
+sudo apt-get upgrade
 # now we can get the whole lot
 sudo apt-get -y install build-essential cmake gcc g++ git unzip pkg-config
 sudo apt-get -y install libjpeg-dev libpng-dev libtiff-dev
@@ -20,3 +20,19 @@ sudo apt-get -y install libopenblas-dev libatlas-base-dev libblas-dev
 sudo apt-get -y install liblapack-dev gfortran libhdf5-dev
 sudo apt-get -y install libprotobuf-dev libgoogle-glog-dev libgflags-dev
 sudo apt-get -y install protobuf-compiler
+
+#sudo apt-get -y update
+#sudo apt-get -y upgrade
+## now we can get the whole lot
+#sudo apt-get -y install build-essential cmake gcc g++ git unzip pkg-config
+#sudo apt-get -y install libjpeg-dev libpng-dev libtiff-dev
+#sudo apt-get -y install libavcodec-dev libavformat-dev libswscale-dev
+#sudo apt-get -y install libgtk2.0-dev libcanberra-gtk*
+#sudo apt-get -y install libxvidcore-dev libx264-dev
+##sudo apt-get -y install python3-dev python3-numpy python3-pip
+#sudo apt-get -y install libtbb2 libtbb-dev libdc1394-22-dev
+#sudo apt-get -y install libv4l-dev v4l-utils
+#sudo apt-get -y install libopenblas-dev libatlas-base-dev libblas-dev
+#sudo apt-get -y install liblapack-dev gfortran libhdf5-dev
+#sudo apt-get -y install libprotobuf-dev libgoogle-glog-dev libgflags-dev
+#sudo apt-get -y install protobuf-compiler
