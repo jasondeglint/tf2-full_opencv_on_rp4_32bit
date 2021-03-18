64e71a7601637cf805722df550b9d7c7201baa9a
jasondeglint <41922820+jasondeglint@users.noreply.github.com>
2021-03-18 16:35:26 -0400

    Create step0_setup.sh

---
+++ step0_setup.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# instructions found here: https://qengineering.eu/install-ubuntu-20.04-on-raspberry-pi-4.html
+
+### basis setup ### 
+
+# to get your administation in order
+sudo apt-get update
+sudo apt-get upgrade
+# now we can get the whole lot
+sudo apt-get install build-essential cmake gcc g++ git unzip pkg-config
+sudo apt-get install libjpeg-dev libpng-dev libtiff-dev
+sudo apt-get install libavcodec-dev libavformat-dev libswscale-dev
+sudo apt-get install libgtk2.0-dev libcanberra-gtk*
+sudo apt-get install libxvidcore-dev libx264-dev
+sudo apt-get install python3-dev python3-numpy python3-pip
+sudo apt-get install libtbb2 libtbb-dev libdc1394-22-dev
+sudo apt-get install libv4l-dev v4l-utils
+sudo apt-get install libopenblas-dev libatlas-base-dev libblas-dev
+sudo apt-get install liblapack-dev gfortran libhdf5-dev
+sudo apt-get install libprotobuf-dev libgoogle-glog-dev libgflags-dev
+sudo apt-get install protobuf-compiler
