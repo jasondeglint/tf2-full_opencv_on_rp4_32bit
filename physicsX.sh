0d1595af53a0689ea6aa5371c47fbfef1afa8ad6
jasondeglint <41922820+jasondeglint@users.noreply.github.com>
2021-03-19 13:14:52 -0400

    Create physicsX.sh

---
+++ physicsX.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+mkdir tensorflowRasp
+cd tensorflowRasp/
+python3 -m pip install virtualenv
+
+virtualenv env
+source env/bin/activate
+
+sudo apt-get install -y libhdf5-dev libc-ares-dev libeigen3-dev gcc gfortran python-dev libgfortran5 libatlas3-base libatlas-base-dev libopenblas-dev libopenblas-base libblas-dev liblapack-dev cython openmpi-bin libopenmpi-dev python3-dev
+sudo pip3 install keras_applications==1.0.8 --no-deps
+sudo pip3 install keras_preprocessing==1.1.0 --no-deps
+sudo pip3 install pybind11
+sudo pip3 install h5py==2.9.0
+sudo pip3 install -U six wheel mock
+wget https://github.com/lhelontra/tensorflow-on-arm/releases/download/v2.1.0/tensorflow-2.1.0-cp37-none-linux_armv7l.whl
+python3 -m pip install tensorflow-2.1.0-cp37-none-linux_armv7l.whl
