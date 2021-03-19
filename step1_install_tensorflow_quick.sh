11c1751d93d3defb41d3c9c67632485c4bb14794
jasondeglint <41922820+jasondeglint@users.noreply.github.com>
2021-03-18 22:46:57 -0400

    Update step1_install_tensorflow_quick.sh

--- step1_install_tensorflow_quick.sh
+++ step1_install_tensorflow_quick.sh
@@ -3,11 +3,11 @@
 # partial instructions from here: https://vielmetti.typepad.com/logbook/2021/02/getting-tensorflow-running-on-arm64.html
 sudo apt-add-repository ppa:deadsnakes/ppa
 sudo apt-get update
-sudo apt-get -y installpython3.7 python3.7-dev
+sudo apt-get -y install python3.7 python3.7-dev
 sudo update-alternatives --install /usr/bin/python python /usr/bin/python3.7 1
 sudo update-alternatives --install /usr/bin/python3 python3 /usr/bin/python3.7 1
 sudo update-alternatives --install /usr/bin/python3 python3 /usr/bin/python3.8 2
-sudo apt-get isntall python3-pip -y
+sudo apt-get install python3-pip -y
 sudo update-alternatives --install /usr/bin/pip pip /usr/bin/pip3 1
 #pip install --upgrade pip
 
