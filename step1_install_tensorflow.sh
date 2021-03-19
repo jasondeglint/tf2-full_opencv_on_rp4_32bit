e670e80433365df60150c97a1de10968e41e78e7
jasondeglint <41922820+jasondeglint@users.noreply.github.com>
2021-03-18 22:54:47 -0400

    Update step1_install_tensorflow.sh

--- step1_install_tensorflow.sh
+++ step1_install_tensorflow.sh
@@ -25,3 +25,33 @@ cmake ..
 make -j4
 sudo make install
 sudo ldconfig
+
+
+### Install TensorFlow or 2.4.1
+# get a fresh start (remember, the 64-bit OS is still under development)
+sudo apt-get -y update
+sudo apt-get -y upgrade
+# install pip and pip3
+sudo apt-get -y install python-pip python3-pip
+# remove old versions, if not placed in a virtual environment (let pip search for them)
+sudo pip uninstall tensorflow
+sudo pip3 uninstall tensorflow
+# install the dependencies (if not already onboard)
+sudo apt-get -y install gfortran
+sudo apt-get -y install libhdf5-dev libc-ares-dev libeigen3-dev
+sudo apt-get -y install libatlas-base-dev libopenblas-dev libblas-dev
+sudo apt-get -y install liblapack-dev
+# upgrade setuptools 47.1.1 -> 53.0.0
+sudo -H pip3 install --upgrade setuptools
+sudo -H pip3 install pybind11
+sudo -H pip3 install Cython==0.29.21
+# install h5py with Cython version 0.29.21 (± 6 min @1950 MHz)
+sudo -H pip3 install h5py==2.10.0
+# install gdown to download from Google drive
+pip3 install gdown
+# copy binairy
+sudo cp /home/ubuntu/.local/bin/gdown /usr/local/bin/gdown
+# download the wheel
+gdown https://drive.google.com/uc?id=1rfgF2U2oZJvQSMbGNZl8f5jbWP4fY6UW
+# install TensorFlow 2.4.1 (± 68 min @1950 MHz)
+sudo -H pip3 install tensorflow-2.4.1-cp38-cp38-linux_aarch64.whl
