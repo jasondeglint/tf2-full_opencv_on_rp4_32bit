f4a7e02bcf9676bb2621ab6933af5964540b5aff
jasondeglint <41922820+jasondeglint@users.noreply.github.com>
2021-03-19 15:32:56 -0400

    Update physicsX.sh

--- physicsX.sh
+++ physicsX.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-mkdir tensorflowRasp
-cd tensorflowRasp/
-python3 -m pip install virtualenv
+#mkdir tensorflowRasp
+#cd tensorflowRasp/
+#python3 -m pip install virtualenv
 
-virtualenv env
-source env/bin/activate
+#virtualenv env
+#source env/bin/activate
 
 sudo apt-get install -y libhdf5-dev libc-ares-dev libeigen3-dev gcc gfortran python-dev libgfortran5 libatlas3-base libatlas-base-dev libopenblas-dev libopenblas-base libblas-dev liblapack-dev cython openmpi-bin libopenmpi-dev python3-dev
 sudo pip3 install keras_applications==1.0.8 --no-deps
@@ -15,4 +15,6 @@ sudo pip3 install h5py==2.9.0
 sudo pip3 install -U six wheel mock
 #wget https://github.com/lhelontra/tensorflow-on-arm/releases/download/v2.1.0/tensorflow-2.1.0-cp37-none-linux_armv7l.whl
 wget https://github.com/lhelontra/tensorflow-on-arm/releases/download/v2.1.0/tensorflow-2.1.0-cp37-none-linux_aarch64.whl
+
+sudo pip3 uninstall tensorflow
 python3 -m pip install tensorflow-2.1.0-cp37-none-linux_aarch64.whl
